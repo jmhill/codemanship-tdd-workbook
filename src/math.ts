897dc3c0f287072bf059625a21dfed818f92e4db
Justin Hill <justin@jmhill.dev>
2025-06-20 11:59:53 -0400

    Remove sample function from deno new project

--- src/math.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A simple function that adds two numbers
- * @param a First number
- * @param b Second number
- * @returns Sum of a and b
- */
-export function add(a: number, b: number): number {
-  return a + b;
-}
