b13b853d0fcc8c7a9faa4a38fe1ab9657a25b401
Justin Hill <justin@jmhill.dev>
2025-06-20 11:59:53 -0400

    add instructions for first few exercises

---
+++ tests/primes_test.ts
@@ -0,0 +1,3 @@
+// Chapter 4 Exercise 1a:
+// Test-Drive some code that will generate prime numbers
+// that are less than 1000
---
+++ tests/roman_numerals_test.ts
@@ -0,0 +1,3 @@
+// Chapter 4 Exercise 1b:
+// Test-drive some code that will convert
+// integers from 1 to 4000 into Roman Numerals
